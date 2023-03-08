1fe2b55551e8652cd9ffc34ef51c80290397ba79
Angelcoder87 <angelaodhiambo87@gmail.com>
2023-03-08 22:44:33 +0300

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,57 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks whether a number is prime
- * @n: the number to check
- *
- * Return: 1 if the number is prime, 0 otherwise
+ * evaluate_num - recursion loop
+ * @num: num
+ * @iterator: number to iterate
+ * Return: return 1 or 0
  */
-int is_prime_number(int n)
+
+int evaluate_num(int num, int iterator)
 {
-	/* Negative numbers, 0, and 1 are not prime */
-	if (n <= 1)
-	return (0);
 
-	/* Check if n is divisible by any number from 2 to n/2 */
-	int i;
-
-	for (i = 2; i <= n / 2; i++)
-
+	if (iterator == num - 1)
 	{
-	if (n % i == 0)
-	return (0); /* n is not prime */
+		return (1);
 	}
 
-	/* n is prime if it is not divisible by any number from 2 to n/2 */
-	return (1);
+	else if (num % iterator == 0)
+	{
+		return (0);
+	}
+
+	if (num % iterator != 0)
+	{
+		return (evaluate_num(num, iterator + 1));
+	}
+
+	return (0);
+
 }
 
+/**
+ * is_prime_number - evaluate prime or not
+ * @num: number
+ * Return: return 1 prime - return 0 otherwise
+ */
+
+int is_prime_number(int num)
+{
+
+	int iterator;
+
+	iterator = 2;
+
+	/* only greater than 2*/
+	if (num < 2)
+	{
+		return (0);
+	}
+
+	if (num == 2)
+	{
+		return (1);
+	}
+
+	return (evaluate_num(num, iterator));
+}
