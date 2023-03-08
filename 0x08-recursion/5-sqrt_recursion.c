5a7bdcbcd57f425417d5aaa4d64b5141463323ed
Angelcoder87 <angelaodhiambo87@gmail.com>
2023-03-08 22:05:40 +0300

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@
  * _sqrt_recursion - calculates the natural square root of a number
  * @n: the number to calculate the square root of
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: the square root of n, or -1 if n does not have natural square root
  */
 int _sqrt_recursion(int n)
 {
@@ -17,14 +17,16 @@ int _sqrt_recursion(int n)
 	int i
 		;
 	for (i = 1; i <= n / 2 ; i++)
+	{
 	/* Check if i*i is equal to n */
 	if (i * i == n)
 	return (i);
+	}
 
 	/* If i*i is greater tahn, n does not have a natural square root */
 	if (i * i > n)
 	return (-1);
-	}
+
 
 	/* If we have not found a square root yet, try the next integer */
 	return (_sqrt_recursion(n - 1));
